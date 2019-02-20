7dc816d0504e20df0912804df432aca212c8c98b
Ryan Nguyen <ryan@Ryans-MBP.hsd1.ut.comcast.net>
2019-02-19 21:53:53 -0700

    wrote some code

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Monster", FALSE);
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Breath Taker", FALSE);
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Heart Attack", FALSE);
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Widow Maker", FALSE);
