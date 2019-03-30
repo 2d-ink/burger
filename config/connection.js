eb16150fd96f1be6270c77cbd5aeec607c58ee3f
Ryan Nguyen <ryan@Ryans-MacBook-Pro-2.local>
2019-03-30 11:46:44 -0600

    fix connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,36 @@
-var mysql = require("mysql"); 
-var connection = mysql.createConnection({
-    host:"localhost", 
-    user: "root", 
-    password: "Formulad1", 
+// var mysql = require("mysql"); 
+// var connection = mysql.createConnection({
+//     host:"localhost", 
+//     user: "root", 
+//     password: "Formulad1", 
+//     database: "burgers_db"
+// })
+
+// connection.connect(function(err){
+//     if(err) throw err; 
+//     console.log("connected as id: " + connection.threadId)
+// });
+
+// module.exports = connection; 
+
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Formulad1",
     database: "burgers_db"
-})
+  });
+};
 
-connection.connect(function(err){
-    if(err)throw err; 
-    console.log("connected as id: " + connection.threadId)
-});
+// Make connection.
+connection.connect()
 
-module.exports = connection; 
+// Export connection for our ORM to use.
+module.exports = connection;
