f5ebf422106da6c8497b2b178795c8a5fcd054b1
Ryan Nguyen <ryan@Ryans-MBP-2.hsd1.ut.comcast.net>
2019-03-29 22:13:20 -0600

    push to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8888; 
+var PORT = process.env.PORT || 3000; 
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
